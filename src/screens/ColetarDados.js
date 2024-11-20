9c95f03553a46fb1e93cbcc93c01798c9bec2046
Nathalia Naomi <naomigraciola@gmail.com>
2024-11-20 08:50:30 -0300

    adicionar votos

--- src/screens/ColetarDados.js
+++ src/screens/ColetarDados.js
@@ -1,18 +1,79 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import globalStyles from '../styles/globalStyles';
 
-export default function ColetarDados({route, navigation}) {
+const ColetarDados = ({route, navigation}) => {
   const {pesquisa} = route.params;
-  
+
+  const handleChange = item => {
+    agradecimentos();
+    setTimeout(function(){abrirPesquisa()}, 3000)
+
+  };
+
+  const ratings = [
+    {
+      id: 'pessimo',
+      label: 'Péssimo',
+      icon: require('../../assets/images/pessimo.png'),
+    },
+    {
+      id: 'ruim',
+      label: 'Ruim',
+      icon: require('../../assets/images/ruim.png'),
+    },
+    {
+      id: 'neutro',
+      label: 'Neutro',
+      icon: require('../../assets/images/neutro.png'),
+    },
+    {
+      id: 'bom',
+      label: 'Bom',
+      icon: require('../../assets/images/bom.png'),
+    },
+    {
+      id: 'excelente',
+      label: 'Excelente',
+      icon: require('../../assets/images/excelente.png'),
+    },
+  ];
+
   const voltar = () => {
-    navigation.goBack()
+    navigation.goBack();
+  };
+
+  const agradecimentos = () => {
+    navigation.navigate('Agradecimento');
   }
-  
+  const abrirPesquisa = () => {
+    navigation.navigate('AcoesPesquisa', {pesquisa: pesquisa});
+  };
   return (
     <View style={globalStyles.container}>
       <Button title="<-" onPress={voltar} />
-      <Text style={globalStyles.header}>O que você achou do {pesquisa.nome}?</Text>
+      <Text style={globalStyles.header}>
+        O que você achou do {pesquisa.nome}?
+      </Text>
+      <FlatList
+        data={ratings}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <TouchableOpacity key={item.id.toString()} onPress={() => handleChange(item)}>
+            <Image source={item.icon} />
+            <Text>{item.label}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
+
+export default ColetarDados;
