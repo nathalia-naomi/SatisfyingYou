910c30fa0beee39e6c10460550a1cd37c29cf30f
Nathalia Naomi <naomigraciola@gmail.com>
2024-11-20 08:49:33 -0300

    adicionar votos

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,23 +10,44 @@ const Home = props => {
       nome: 'Meninas Digitais CPU',
       data: '01/04/2022',
       imagem: require('../../assets/images/meninas-digitais.png'),
+      ratings: [
+        {id: 1, value: 'pessimo', count: 0},
+        {id: 2, value: 'ruim', count: 0},
+        {id: 3, value: 'neutro', count: 0},
+        {id: 4, value: 'bom', count: 0},
+        {id: 5, value: 'excelent', count: 0}
+      ]
     },
     {
       id: '2',
       nome: 'Secomp 2023',
       data: '1010/2023',
       imagem: require('../../assets/images/secomp.png'),
+      ratings: [
+        {id: 1, value: 'pessimo', count: 0},
+        {id: 2, value: 'ruim', count: 0},
+        {id: 3, value: 'neutro', count: 0},
+        {id: 4, value: 'bom', count: 0},
+        {id: 5, value: 'excelent', count: 0}
+      ]
     },
     {
       id: '3',
       nome: 'Ubuntu 2022',
       data: '05/06/2022',
       imagem: require('../../assets/images/ubuntu.png'),
+      ratings: [
+        {id: 1, value: 'pessimo', count: 0},
+        {id: 2, value: 'ruim', count: 0},
+        {id: 3, value: 'neutro', count: 0},
+        {id: 4, value: 'bom', count: 0},
+        {id: 5, value: 'excelent', count: 0}
+      ]
     },
   ];
 
   const abrirPesquisa = item => {
-    props.navigation.navigate('AcoesPesquisa', {props:props, pesquisa: item});
+    props.navigation.navigate('AcoesPesquisa', {pesquisa: item});
   };
 
   const novaPesquisa = () => {
